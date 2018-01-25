edc5de6f777264ea5ec88c6ea9b92daadc4622e5
Vanessa Bandeira <van3ssabandeira@gmail.com>
2018-01-25 07:18:00 -0300

    controller e fxml de teste

--- TrocoGuloso/src/application/Main.java
+++ TrocoGuloso/src/application/Main.java
@@ -1,19 +1,18 @@
 package application;
 	
 import javafx.application.Application;
-import javafx.stage.Stage;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
-import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
 
 
 public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("Main.fxml"));
-			Scene scene = new Scene(root,400,400);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			BorderPane root = FXMLLoader.load(this.getClass().getResource("Main.fxml"));
+			Scene scene = new Scene(root);
 			primaryStage.setScene(scene);
 			primaryStage.show();
 		} catch(Exception e) {
--- TrocoGuloso/src/application/TrocoGulosoController.java
+++ TrocoGuloso/src/application/TrocoGulosoController.java
@@ -1,5 +1,67 @@
 package application;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
 public class TrocoGulosoController {
-	
+
+	@FXML
+	private Label res;
+
+	@FXML
+	private TextField conta;
+
+	@FXML
+	private TextField Pagamento;
+
+	@FXML
+	private Button btnCalcular;
+
+	@FXML
+	void procuraTroco(ActionEvent event) {
+		if (Double.parseDouble(Pagamento.getText()) < Double.parseDouble(conta.getText())) {
+			res.setText("\nPagamento insuficiente, faltam R$"+ String.format("%.2f", Double.parseDouble(conta.getText()) - Double.parseDouble(Pagamento.getText())));
+		}else {
+			int nota[] = {100, 50, 20, 10, 5, 2, 1};
+			int centavos[] = {50, 25, 10, 5, 1};
+
+			String result;
+			double troco;
+			int i, vlr, ct;
+
+			troco = Double.parseDouble(Pagamento.getText()) - Double.parseDouble(conta.getText());
+			result ="\nTroco = R$"+ String.format("%.2f", troco) +"\n\n";
+			
+			vlr = (int)troco;
+			
+			i = 0;
+			
+			while (vlr != 0) {
+				ct = vlr / nota[i]; // calculando a qtde de notas
+				if (ct != 0) {
+					result = result + (ct +"nota(s) de R$"+ nota[i] +"\n");
+					vlr = vlr % nota[i]; // sobra
+				}
+				i = i + 1; // pr�xima nota
+			}
+
+			result = result +"\n";
+
+			vlr = (int)Math.round((troco - (int)troco) * 100);
+			i = 0;
+			while (vlr != 0) {
+				ct = vlr / centavos[i]; // calculando a qtde de moedas
+				if (ct != 0) {
+					result = result + (ct +"moeda(s) de"+ centavos[i] +
+							"centavo(s)\n");
+					vlr = vlr % centavos[i]; // sobra
+				}
+				i = i + 1; // pr�ximo centavo
+			}
+			this.res.setText(result);
+		}
+	}
 }
