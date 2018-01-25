282b4da792d6832d3eda26cf07821c0711099a5b
Vanessa Bandeira <van3ssabandeira@gmail.com>
2018-01-25 08:21:11 -0300

    troca do FXML(main->trocoguloso)

--- TrocoGuloso/src/application/Main.java
+++ TrocoGuloso/src/application/Main.java
@@ -11,7 +11,7 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = FXMLLoader.load(this.getClass().getResource("Main.fxml"));
+			BorderPane root = FXMLLoader.load(this.getClass().getResource("TrocoGuloso.fxml"));
 			Scene scene = new Scene(root);
 			primaryStage.setScene(scene);
 			primaryStage.show();
